38fcb2f529a6cd49c044db5cdf69c4310319faa6
Fayaz2544 <fayaz.heembu2544@gmail.com>
2024-05-09 13:53:55 +0700

    edit approve

--- routes/approve.js
+++ routes/approve.js
@@ -2,17 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 const shopSchema = require('../models/shop');
+const verfyToken = require('../middleware/auth.sh.middleware')
 
-router.put("/:id", async function (req, res, next) {
+
+router.put("/:id", verfyToken, async function (req, res, next) {
   try {
-    let { username, password, firstName, lastName, email, role, token, status } = req.body;
 
-    let approve = await shopSchema.findByIdAndUpdate(req.params.id, { username, password, firstName, lastName, email, role, token, status }, { new: true });
-    return res.status(200).send({
-      data: approve,
-      message: "อนุมัติสำเร็จ",
-      success: true,
-    });
+    const role = req.auth.role;
+
+    if(role === 'admin'){
+
+      let { username, password, firstName, lastName, email, role, token, status } = req.body;
+
+      let approve = await shopSchema.findByIdAndUpdate(req.params.id, { username, password, firstName, lastName, email, role, token, status }, { new: true });
+      return res.status(200).send({
+        data: approve,
+        message: "อนุมัติสำเร็จ",
+        success: true,
+      });
+    }
   } catch (error) {
     res.status(500).send({
       message: "ไม่พบข้อมูล",
